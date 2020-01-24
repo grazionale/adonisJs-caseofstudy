9a9adbb19cf04690b91fe5fa1afc42ef8380c507
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-24 15:47:31 -0300

    controller user e config eslint

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async store ({request}) { // request -> contexto de uma requisição, parecido com req, e res usado no Express
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
+  }
+}
+
+module.exports = UserController
