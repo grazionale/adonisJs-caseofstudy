d51e66b0e0bb851cb53ad85d8919a4332e7b2798
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-27 09:47:11 -0300

    disparo de e-mail

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = require('../../Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -13,6 +14,13 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        (message) => {
+          message.from('foo@bar.com').to(user.email).subject('Recuperação de senha')
+      })
     } catch (error) {
       return response.status(error.status).send({ error: { message: 'Algo não deu certo, esse e-mail existe?' }})
     }
