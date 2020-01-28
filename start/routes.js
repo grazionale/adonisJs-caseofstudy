1e07c39c2d921ffa2d5a937ad74df2379cf1dced
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-28 17:37:33 -0300

    visualizando arquivo upado

--- start/routes.js
+++ start/routes.js
@@ -7,4 +7,6 @@ Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 Route.post('files', 'FileController.store')
+Route.get('/files/:id', 'FileController.show')
+
 
