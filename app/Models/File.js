ca3ff48388a38dae166c25aa6ce94303994606fc
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-28 17:42:40 -0300

    adicionando campo virtual (url) nos arquivos upados

--- app/Models/File.js
+++ app/Models/File.js
@@ -1,9 +1,21 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Env = use('Env')
+
 const Model = use('Model')
 
 class File extends Model {
+  /**
+   * Campo virtual. Não está na migration e não vai para o banco.
+   * Este campo url em específico, servirá para fornecer uma URL ao arquivo
+   */
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl({ id }) {
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
 }
 
 module.exports = File
