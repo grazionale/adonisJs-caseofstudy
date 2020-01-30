2fda6a2ce7fc4af50e346535d89b616cb919bd85
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-30 16:39:52 -0300

    CRUD projetos

--- start/routes.js
+++ start/routes.js
@@ -6,9 +6,17 @@ Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
-Route.post('files', 'FileController.store')
-Route.get('/files/:id', 'FileController.show')
 Route.get('/files/', 'FileController.index')
+Route.get('/files/:id', 'FileController.show')
 
-
-
+/**
+ * Agrupa as rotas que precisam passar por determinado middleware
+ */
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+  /**
+   * Projects - Utilizando resoucer, é possível mapear todas as rotas
+   * automaticamente
+   */
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
