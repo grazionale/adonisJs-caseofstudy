efe6a484102a717df9919c71714610201344b023
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-31 12:20:18 -0300

    Utilizando transactions

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,18 +1,23 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Database = use('Database')
 
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password']) // only busca os campos definidos no array dentro da req
     const addresses = request.input('addresses')
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction() // Iniciando uma transação
+
+    const user = await User.create(data, trx) // utilizando a tarnsação
 
     /**
      * Após criar o usuário, cria-se os endereços dele.
      */
-    await user.addresses().createMany(addresses)
+    await user.addresses().createMany(addresses, trx) // utilizando a tarnsação
+
+    await trx.commit() // Comitando transação
 
     return user
   }
